a45686e344b3c46885de0a7410cbc7db1d42a1cf
Benji <97198967+BenPI88@users.noreply.github.com>
2023-04-29 16:36:40 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, socket
 if len(sys.argv) == 0 or len(sys.argv) == 1:
   print("Please specify the instance the host is using with the following command:\npython3 client.py <ip> <port>")
 elif len(sys.argv) == 2:
-  os.system(f"nc {sys.argv[0]} {sys.argv[1]} -v -o pclink")
+  i = 0
+  id = ""
+  while not i == 10:
+    i += 1
+    id = id + string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase) - 1)]
+  os.system(f"nc {sys.argv[0]} {sys.argv[1]} -v -o pclink-{id}")
+  os.system(f"ssh-copy-id -i pclink-{id} {os.getlogin()}@{socket.gethostname()}")
 else:
   print("Too many arguments!")
